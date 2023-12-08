4a8d76f2bd885ea2de7b01dfd7f7b7829c57bea6
janakiramprasad <janakiramprasad145@gmail.com>
2023-12-08 21:21:23 +0530

    first commit

---
+++ app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from app.models import *
+
+# Register your models here.
+
+admin.site.register(Country)
+
+admin.site.register(Capital)
+
+admin.site.register(Dept)
+
+admin.site.register(Emp)
---
+++ app/models.py
@@ -0,0 +1,38 @@
+from typing import Any
+from django.db import models
+
+# Create your models here.
+class Country(models.Model):
+    country_ID=models.IntegerField(primary_key=True)
+    country_name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.country_name
+    
+class Capital(models.Model):
+    country_ID=models.OneToOneField(Country, on_delete=models.CASCADE)
+    capital=models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.capital
+
+
+
+class Dept(models.Model):
+    Dept_no=models.IntegerField(primary_key=True)
+    dname=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.dname
+    
+class Emp(models.Model):
+    empno=models.IntegerField(primary_key=True)
+    ename=models.CharField(max_length=100)
+    Dept_no=models.OneToOneField(Dept,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ename
+    
+
+
+        
